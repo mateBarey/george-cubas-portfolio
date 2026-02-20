cfd07242091652db9ae91d9f0745092339e8bbda
George <georgeraulc@gmail.com>
2026-02-20 15:54:39 -0600

    changed app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Cloudflare Web Analytics */}
+        <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "ce9ff04f773b4601adc9952ebde0163e"}'></script>
+        {/* End Cloudflare Web Analytics */}
+      </head>
       <body className="font-sans antialiased">
         {children}
         <Analytics />
